5251cb18177e575efaeb90dbfcfb1ac776574714
BhanuPrakash-123 <131601551+BhanuPrakash-123@users.noreply.github.com>
2025-03-23 22:48:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -102,4 +102,5 @@ def redirect_to_original(short_code):
     return redirect(document['long_url'], code=302)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.getenv('PORT', 5000))  # Use PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port)
